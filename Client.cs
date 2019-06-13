e957edbff239ea4354bd6fe175917a4b729e3a53
jesus-montano <jmontano@nearsoft.com>
2019-06-13 17:30:14 -0600

    refactor Create class Client and Comments add all methods in Client, make an instance of Client in Program and put the methods

---
+++ Client.cs
@@ -0,0 +1,87 @@
+using System;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Net.Http.Headers;
+namespace myApp
+{
+    public class Client : HttpClient {
+        public Client(){
+            this.BaseAddress = new Uri("https://jsonplaceholder.typicode.com/");
+            this.DefaultRequestHeaders.Accept.Clear();
+            this.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("aplicatiion/json"));
+
+        }
+
+       public async Task<Comment> GetById( string id){
+            //get Comment
+            try{
+                HttpResponseMessage response = await this.GetAsync("comments/" + id);
+                response.EnsureSuccessStatusCode();
+
+                Comment comment = await response.Content.ReadAsAsync<Comment>();
+
+                //Console.WriteLine("name: {0}\nemail: {1}\nbody: {2}", comment.name, comment.email, comment.body);
+                return comment;
+
+            }
+            catch(HttpRequestException e){
+                Console.WriteLine("{0}", e.Message);
+                return null;
+            }
+        }
+
+       public async Task<List<Comment>> GetAll(){
+            //get Comment
+            try{
+                HttpResponseMessage response = await this.GetAsync("comments" );
+                response.EnsureSuccessStatusCode();
+
+                List<Comment> comments = await response.Content.ReadAsAsync<List<Comment>>();
+
+                foreach(Comment comment in comments){
+                    Console.WriteLine("name: {0}\nemail: {1}\nbody: {2}", comment.name, comment.email, comment.body);
+                }
+                return comments;
+            }
+            catch(HttpRequestException e){
+                Console.WriteLine("{0}", e.Message);
+                return null;
+            }
+        }
+
+       public async Task<Comment> postComment(Comment comment){
+            //post Comment
+            try{
+                HttpResponseMessage response = await this.PostAsJsonAsync("comments", comment);
+                response.EnsureSuccessStatusCode();
+
+                Comment comments = await response.Content.ReadAsAsync<Comment>();
+                Console.WriteLine("saved");
+                return comments;
+
+            }
+            catch(HttpRequestException e){
+                Console.WriteLine("{0}", e.Message);
+                return null;
+            }
+        }
+
+        public async Task<Comment> updateComment(string id, Comment comment){
+            //put Comment
+            try{
+                HttpResponseMessage response = await this.PutAsJsonAsync("comments/"+ id, comment);
+                response.EnsureSuccessStatusCode();
+
+                Comment comments = await response.Content.ReadAsAsync<Comment>();
+                Console.WriteLine("updated");
+                return comments;
+
+            }
+            catch(HttpRequestException e){
+                Console.WriteLine("{0}", e.Message);
+                return null;
+            }
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,103 +1,27 @@
 ﻿using System;
 using System.Threading.Tasks;
-using System.Collections.Generic;
-using System.Net.Http;
-using System.Net.Http.Headers;
-
 namespace myApp
 {
     class Program
     {
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
            
-            runClient();
+           await runClient();
         }
-        static void runClient(){
-            using(var client = new HttpClient()){
-                client.BaseAddress =new Uri("https://jsonplaceholder.typicode.com/");
-                client.DefaultRequestHeaders.Accept.Clear();
-                client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("aplicatiion/json"));
-                GetById(client, "1").Wait();
-                postComment(client, new Comment() {name = "asdgfs", email = "test@test.com", body ="dgadfgshfgnsvrgsbfdd"}).Wait();
-                updateComment(client, "1", new Comment() {name = "asdgfs", email = "test@test.com", body ="dgadfgshfgnsvrgsbfdd"}).Wait();
-                 GetAll(client).Wait();
+        static async Task runClient(){
+            Client client = new Client();
+                Comment comment = new Comment(); 
+                comment = await client.GetById("1");
+                Console.WriteLine(comment.name.ToString());
+                //client.postComment(new Comment() {name = "asdgfs", email = "test@test.com", body ="dgadfgshfgnsvrgsbfdd"}).Wait();
+                //client.updateComment("1", new Comment() {name = "asdgfs", email = "test@test.com", body ="dgadfgshfgnsvrgsbfdd"}).Wait();
+                //client.GetAll().Wait();
 
-            }
+            
         }
-        static async Task GetById(HttpClient client, string id){
-            //get Comment
-            try{
-                HttpResponseMessage response = await client.GetAsync("comments/" + id);
-                response.EnsureSuccessStatusCode();
-
-                Comment comment = await response.Content.ReadAsAsync<Comment>();
-
-                Console.WriteLine("name: {0}\nemail: {1}\nbody: {2}", comment.name, comment.email, comment.body);
-
-            }
-            catch(HttpRequestException e){
-                Console.WriteLine("{0}", e.Message);
-            }
-        }
-
-        static async Task GetAll(HttpClient client){
-            //get Comment
-            try{
-                HttpResponseMessage response = await client.GetAsync("comments" );
-                response.EnsureSuccessStatusCode();
-
-                List<Comment> comments = await response.Content.ReadAsAsync<List<Comment>>();
-
-                foreach(Comment comment in comments){
-                    Console.WriteLine("name: {0}\nemail: {1}\nbody: {2}", comment.name, comment.email, comment.body);
-                }
-            }
-            catch(HttpRequestException e){
-                Console.WriteLine("{0}", e.Message);
-            }
-        }
-
-        static async Task postComment(HttpClient client, Comment comment){
-            //post Comment
-            try{
-                HttpResponseMessage response = await client.PostAsJsonAsync("comments", comment);
-                response.EnsureSuccessStatusCode();
-
-                Comment comments = await response.Content.ReadAsAsync<Comment>();
-                Console.WriteLine("saved");
-
-            }
-            catch(HttpRequestException e){
-                Console.WriteLine("{0}", e.Message);
-            }
-        }
-
-        static async Task updateComment(HttpClient client, string id, Comment comment){
-            //put Comment
-            try{
-                HttpResponseMessage response = await client.PutAsJsonAsync("comments/"+ id, comment);
-                response.EnsureSuccessStatusCode();
-
-                Comment comments = await response.Content.ReadAsAsync<Comment>();
-                Console.WriteLine("updated");
-
-            }
-            catch(HttpRequestException e){
-                Console.WriteLine("{0}", e.Message);
-            }
-        }
-    }
-
-    public class Comment{
-        public int postId {get; set;}
-        public int id {get; set;}
         
-        public string name {get; set;}
-
-        public string email {get; set;}
-    
-        public string body {get; set;}
     }
+
 }
    
